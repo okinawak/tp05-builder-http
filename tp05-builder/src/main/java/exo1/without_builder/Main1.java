5efba061ba3ff01396f53ddd5ec7be61e36da1a3
okinawak <38444276+okinawak@users.noreply.github.com>
2025-11-26 11:24:08 +0100

    exo 1 question 1

--- tp05-builder/src/main/java/exo1/without_builder/Main1.java
+++ tp05-builder/src/main/java/exo1/without_builder/Main1.java
@@ -1,10 +1,44 @@
 package exo1.without_builder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main1 {
+    public static void main(String[] args) {
+        
+        // Requête 1 : GET /users?limit=10
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("limit", "10");
+        
+        
+        HttpRequest req1 = new HttpRequest(
+            "GET", 
+            "/users", 
+            "api.mon-site.com", 
+            null, 
+            queryParams, 
+            null, 
+            true, 
+            false, 
+            10000
+        );
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        // Requête 2 : POST /login avec un body JSON
+        String jsonBody = "{\"username\": \"user1\", \"password\": \"pass123\"}";
+        
+        HttpRequest req2 = new HttpRequest(
+            "POST", 
+            "/login", 
+            "api.mon-site.com", 
+            null, 
+            null, 
+            jsonBody, 
+            false, 
+            true, 
+            10000
+        );
+        
+        System.out.println(req1);
+        System.out.println(req2);
+    }
 }
